5c5c27761874cad62b93757451f276742c074b41
Suyash <paradisestrings99@gmail.com>
2022-08-09 09:47:07 +0530

    initial commit

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const path = require("path");
+
+const express = require("express");
+
+const rootDir = require("../util/path");
+
+const router = express.Router();
+
+const products = [];
+
+router.get("/add-product", (req, res, next) => {
+	res.render("add-product", {
+		pageTitle: "Add Product",
+		path: "/admin/add-product",
+		activeAddProduct: true,
+		formsCSS: true,
+		productCSS: true
+	});
+});
+
+router.post("/add-product", (req, res, next) => {
+	products.push({ title: req.body.title });
+	res.redirect("/");
+});
+
+module.exports = { routes: router, products: products };
