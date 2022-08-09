a86221d2335c56eba605eb7a009389f8835f483b
Suyash <paradisestrings99@gmail.com>
2022-08-09 10:23:41 +0530

    added controllers

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const path = require("path");
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const adminData = require("./routes/admin");
+const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
+
+const errorsController = require("./controllers/errors");
 
 const app = express();
 
@@ -13,11 +15,9 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminData.routes);
+app.use("/admin", adminRouter);
 app.use(shopRouter);
 
-app.use((req, res, next) => {
-	res.status(404).render("404", { pageTitle: "Page Not Found" });
-});
+app.use(errorsController.notFound);
 
 app.listen(3000);
